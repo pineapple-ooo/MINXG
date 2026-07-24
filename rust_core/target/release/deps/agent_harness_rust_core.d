b9ba7db96edddb3a0158a618dd9a0ab3bbe87eeb
Your Name <you@example.com>
2026-07-25 02:40:43 +0800

    chore: remove junk files from git index (pyc, target, egg-info)

--- rust_core/target/release/deps/agent_harness_rust_core.d
+++
@@ -1,7 +0,0 @@
-/storage/emulated/0/MINXG v0.18.5/rust_core/target/release/deps/minxg_rust_core.d: src/lib.rs
-
-/storage/emulated/0/MINXG v0.18.5/rust_core/target/release/deps/libminxg_rust_core.so: src/lib.rs
-
-/storage/emulated/0/MINXG v0.18.5/rust_core/target/release/deps/libminxg_rust_core.rlib: src/lib.rs
-
-src/lib.rs:
--- rust_core/target/release/libagent_harness_rust_core.d
+++
@@ -1,1 +0,0 @@
-/storage/emulated/0/MINXG\ v0.18.5/rust_core/target/release/libminxg_rust_core.rlib: /storage/emulated/0/MINXG\ v0.18.5/rust_core/src/lib.rs
